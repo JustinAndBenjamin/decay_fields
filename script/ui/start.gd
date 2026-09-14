578320445390b3942f582b64281cc872291f6c37
JustinCase-306 <friedrich.fnz@gmail.com>
2026-09-14 16:21:24 +0200

    Add intro video to start screen
    This commit replaces the old start transition with a fullscreen intro video and ambient music on the start screen. It adds fade-out logic for the intro*, restores music volume after the video ends, and disables the obsolete start animation script that is no longer used.

--- script/ui/start.gd
+++ script/ui/start.gd
@@ -3,23 +3,52 @@ extends Control
 @export var scene_1 : PackedScene
 @export var scene_2 : PackedScene
 @export var scene_3 : PackedScene
+@onready var video_player = $dfintro/AspectRatioContainer/VideoStreamPlayer
+@onready var intro_node = $dfintro
+@onready var music_player = $AudioStreamPlayer
 
-
+func _ready() -> void:
+	if music_player:
+		music_player.volume_db = -80.0
+	
+	if video_player:
+		get_tree().create_timer(4.2).timeout.connect(_on_intro_video_finished)
+	else:
+		push_error("Error: VideoPlayer not found. Please contact the developers.")
 
 func _on_monpok_map_pressed() -> void:
 	if scene_1:
 		get_tree().change_scene_to_packed(scene_1)
-
+		
+func _on_intro_video_finished() -> void:
+	var fade_overlay = $dfintro/dfintro_fade
+	
+	if fade_overlay and intro_node:
+		fade_overlay.visible = true
+		fade_overlay.modulate.a = 1.0
+		
+		if video_player:
+			video_player.stop()
+			video_player.visible = false
+		
+		var tween = create_tween()
+		tween.tween_property(fade_overlay, "modulate:a", 0.0, 1.2)
+		
+		if music_player:
+			var music_tween = create_tween()
+			music_tween.tween_property(music_player, "volume_db", 0.0, 1.2)
+		
+		tween.finished.connect(func():
+			intro_node.queue_free()
+		)
 
 func _on_npc_test_pressed() -> void:
 	if scene_2:
 		get_tree().change_scene_to_packed(scene_2)
 
-
 func _on_house_map_pressed() -> void:
 	if scene_3:
 		get_tree().change_scene_to_packed(scene_3)
-
 
 func _on_laden_pressed() -> void:
 	var saved_data = SaveSystem.load_game()
--- script/ui/start_anim.gd
+++ script/ui/start_anim.gd
@@ -1,23 +1,23 @@
-extends CanvasLayer
-
-@onready var transition_black: TextureRect = $transition_black
-
+#extends CanvasLayer
+#
+#@onready var transition_black: TextureRect = $transition_black
+#
 # Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	transition_black.visible=false
-	anim_play("res://Scenes/start.tscn")
+#func _ready() -> void:
+#	transition_black.visible=false
+#	anim_play("res://Scenes/start.tscn")
 	
 # Animation
 
-func anim_play(next_scene_path: String = "") -> void:
-	transition_black.modulate=Color(1, 1, 1, 1.0)
-	transition_black.visible=true
+#func anim_play(next_scene_path: String = "") -> void:
+#	transition_black.modulate=Color(1, 1, 1, 1.0)
+#	transition_black.visible=true
 	
-	var anim_tween = create_tween().set_parallel(false)
-	anim_tween.tween_property(transition_black, "modulate:a", 0.0, 3.0)
+#	var anim_tween = create_tween().set_parallel(false)
+#	anim_tween.tween_property(transition_black, "modulate:a", 0.0, 3.0)
 	
-	await anim_tween.finished
-	transition_black.visible=false
+#	await anim_tween.finished
+#	transition_black.visible=false
 	
 	# old code (for start_anim), not needed anymore
 	#if next_scene_path != "":
