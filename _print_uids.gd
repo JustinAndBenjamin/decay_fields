c9834041e68b86ee118fcd18b8a093100c7ff574
JustinCase-306 <friedrich.fnz@gmail.com>
2026-09-14 20:09:12 +0200

    Bugfix #02*

---
+++ _print_uids.gd
@@ -0,0 +1,13 @@
+extends SceneTree
+
+func _init() -> void:
+	var paths := [
+		"res://scenes/world/maps/map_world_monpok.res",
+		"res://scenes/world/maps/map_house_start.res",
+		"res://scenes/ui/start.tscn",
+	]
+	for p in paths:
+		var id := ResourceLoader.get_resource_uid(p)
+		var txt := ResourceUID.id_to_text(id) if id != -1 else "(no uid)"
+		print(p, " -> ", txt, "  (id=", id, ")")
+	quit(0)
