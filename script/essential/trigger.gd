812ec4d400cce33b0e311a8ef6311429657b6b1e
Benjamin-306 <github@benjamin.fudeus.net>
2026-09-17 18:04:22 +0200

    Add trigger logic and try in house map (#182)

--- script/essential/trigger.gd
+++ script/essential/trigger.gd
@@ -1,17 +1,19 @@
 extends Node2D
 
-
+@export var scene: PackedScene
+@export var tutorial : String
+@export var anim : AnimatedSprite2D
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	print("Trigger is ready")
 
-func trigger() -> void:
-	pass
+func trigger(body) -> void:
+	if body.name == "Player":
+		if scene:
+			get_tree().change_scene_to_packed(scene)
+		if anim:
+			anim.play()
 
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
-	trigger()
-
-
-func _on_area_2d_area_entered(area: Area2D) -> void:
-	trigger()
+	trigger(body)
